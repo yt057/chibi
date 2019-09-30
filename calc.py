f2460a3adf0b1293dd32e3a9d00898ea6762d82b
TAKAGI Yui <21716057@jwu.ac.jp>
2019-09-30 16:45:38 +0900

    calc(1+2+3)

--- calc.py
+++ calc.py
@@ -1,8 +1,9 @@
 def calc(s):
     print('s=',s)
     nums = map(int,s.split('+'))
-    print('nums=',nums)
+    #print('nums=',nums)
     return sum(nums)
 
-
+print(calc("1"))
 print(calc("1+2"))
+print(calc("1+2+3"))
