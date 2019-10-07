0e53140ca59a5db3da37d06dea11c0323226bc4f
TAKAGI Yui <21716057@jwu.ac.jp>
2019-10-07 16:45:35 +0900

    all operators

--- rational.py
+++ rational.py
@@ -1,5 +1,5 @@
 class Q(object):
-    def __init__(self, a, b=1):
+    def __init__(self, a, b):
         self.a = a
         self.b = b
     def __repr__(self):
@@ -12,9 +12,31 @@ class Q(object):
         c = q.a
         d = q.b 
         return Q(a*d+b*c, b*d)
+    def __sub__(self,q):
+        a = self.a
+        b = self.b
+        c = q.a
+        d = q.b 
+        return Q(a*d-b*c, b*d)
+    def __mul__(self,q):
+        a = self.a
+        b = self.b
+        c = q.a
+        d = q.b 
+        return Q(a*d-b*c, b*d)
+    def __truediv__(self,q):
+        a = self.a
+        b = self.b
+        c = q.a
+        d = q.b 
+        return Q(a*d,b*c)
 
 
 
 q1= Q(1,2)
-q2=Q(1,3)
+q2= Q(1,3)
 print(q1 + q2) #==>5/6
+print(q1 - q2) #   1/6
+print(q1 * q2) #   1/6
+print(q1 / q2) #   3/2
+
