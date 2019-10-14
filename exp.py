72a29d89680da237e50526664c9dbb3767ba5f09
TAKAGI Yui <21716057@jwu.ac.jp>
2019-10-14 16:10:12 +0900

    Add

--- exp.py
+++ exp.py
@@ -11,3 +11,20 @@ class Val(object):
 v = Val(1)
 print(v)
 assert v.eval() == 1
+
+class Add(object):
+    __slots__ =['left','right']
+    def __init__(self,a,b):
+        self.left = a
+        self.right = b
+    def eval(self):
+        return self.left.eval() + self.right.eval()
+
+
+e = Add(Val(1),Val(2))
+assert e.eval() == 3
+print(e.eval())
+
+e = Add(Val(1),Add(Val(2),Val(3)))
+assert e.eval() == 6
+print(e.eval())
