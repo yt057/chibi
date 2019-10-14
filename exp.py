5cb08e20d15bd6baec9f2aca30f2e2bd67528056
TAKAGI Yui <21716057@jwu.ac.jp>
2019-10-14 16:42:48 +0900

    Expr

--- exp.py
+++ exp.py
@@ -1,5 +1,8 @@
 
-class Val(object):
+class Expr(object):
+        pass
+
+class Val(Expr):
     __slots__= ['value']
     def __init__(self, value = 0):
         self.value=value 
@@ -12,18 +15,28 @@ v = Val(1)
 print(v)
 assert v.eval() == 1
 
-class Add(object):
+assert isinstance(v, Expr)
+assert isinstance(v, Val)
+assert not isinstance(v, int)
+
+def toExpr(a):
+    if not isinstance(a,Expr):
+        a = Val(a)
+    return a
+
+class Add(Expr):
     __slots__ =['left','right']
-    def __init__(self,a,b):
-        self.left = a
-        self.right = b
+    def __init__(self, a, b):
+        self.left = toExpr(a)
+        self.right = toExpr(b)
     def eval(self):
         return self.left.eval() + self.right.eval()
 
 
-e = Add(Val(1),Val(2))
-assert e.eval() == 3
+e=Add(1,Add(1,2))
 print(e.eval())
+assert e.eval() == 4
+
 
 e = Add(Val(1),Add(Val(2),Val(3)))
 assert e.eval() == 6
